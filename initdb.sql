7998f5e03f8b5e0e933aa2d1f19f30b810fb54cd
Faraz Fallahi <fffaraz@gmail.com>
2019-03-04 17:22:42 -0600

    Update initdb.sql

--- initdb.sql
+++ initdb.sql
@@ -1,22 +1,26 @@
 CREATE EXTENSION multicorn;
 
 CREATE SERVER csv_srv foreign data wrapper multicorn options (
-    wrapper 'multicorn.csvfdw.CsvFdw'
+  wrapper 'multicorn.csvfdw.CsvFdw'
 );
 
 CREATE FOREIGN TABLE csvtest (
-       year numeric,
-       make character varying,
-       model character varying,
-       length numeric
+  year numeric,
+  make character varying,
+  model character varying,
+  length numeric
 ) server csv_srv options (
-       filename '/data/test.csv',
-       skip_header '1',
-       delimiter ',');
+  filename '/data/test.csv',
+  skip_header '1',
+  delimiter ','
+);
+
+CREATE SERVER myfdw_srv foreign data wrapper multicorn options (
+  wrapper 'myfdw.ConstantForeignDataWrapper'
+);
 
 CREATE FOREIGN TABLE constanttable (
     test character varying,
     test2 character varying
-) server multicorn_srv options (
-    wrapper 'myfdw.ConstantForeignDataWrapper'
-)
+) server myfdw_srv options (
+);

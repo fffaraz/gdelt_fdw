45388e96d4999281583b250768250a236d6cd2c8
Faraz Fallahi <fffaraz@gmail.com>
2019-03-05 10:30:50 -0600

    gzip

--- fdw/gdelt_fdw/__init__.py
+++ fdw/gdelt_fdw/__init__.py
@@ -1,15 +1,19 @@
 from multicorn import ForeignDataWrapper
 from multicorn.utils import log_to_postgres
+import csv
+import gzip
 
 class GdeltForeignDataWrapper(ForeignDataWrapper):
 
-    def __init__(self, options, columns):
-        super(GdeltForeignDataWrapper, self).__init__(options, columns)
-        self.columns = columns
+	def __init__(self, options, columns):
+		super(GdeltForeignDataWrapper, self).__init__(options, columns)
+		self.options = options
+		self.columns = columns
 
-    def execute(self, quals, columns):
-        for index in range(20):
-            line = {}
-            for column_name in self.columns:
-                line[column_name] = '%s %s' % (column_name, index)
-            yield line
+	def execute(self, quals, columns):
+		# http://data.gdeltproject.org/events/index.html
+		# https://github.com/dialogbox/py_csvgz_fdw
+		with gzip.open('/data/20190303.export.CSV.zip', 'rt') as stream:
+			reader = csv.reader(stream, delimiter='\t')
+			for line in reader:
+				yield line;
--- fdw/setup.py
+++ fdw/setup.py
@@ -2,9 +2,9 @@ import subprocess
 from setuptools import setup, find_packages, Extension
 
 setup(
-  name='gdelt_fdw',
-  version='1.0.0',
-  author='Faraz Fallahi',
-  license='Postgresql',
-  packages=['gdelt_fdw']
+	name='gdelt_fdw',
+	version='1.0.0',
+	author='Faraz Fallahi <fffaraz@gmail.com>',
+	license='Postgresql',
+	packages=['gdelt_fdw']
 )

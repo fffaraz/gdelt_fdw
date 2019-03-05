23c9d8876b0ea9c6a0b0b78b37425148c293d4fe
Faraz Fallahi <fffaraz@gmail.com>
2019-03-05 11:45:45 -0600

    Update __init__.py

--- fdw/gdelt_fdw/__init__.py
+++ fdw/gdelt_fdw/__init__.py
@@ -22,15 +22,25 @@ class GdeltForeignDataWrapper(ForeignDataWrapper):
 			urllib.urlretrieve(url, filename)
 			if os.path.getsize(filename) < 1:
 				os.remove(filename)
+				return ''
+		return filename
 
 	def execute(self, quals, columns):
-		for d in xrange(1,10):
-			yesterday = datetime.datetime.now() - datetime.timedelta(days = d)
-			self.download(yesterday.strftime('%Y%m%d'))
-		files = glob.glob('/data/*.export.CSV.zip')
-		for filename in files:
-			with zipfile.ZipFile(filename) as myzip:
-				with myzip.open(filename[6:-4]) as stream:
-					reader = csv.reader(stream, delimiter='\t')
-					for line in reader:
-						yield line;
+		mindate = '20130401'
+		filedates = []
+		for qual in quals:
+			if qual.field_name == 'sqldate' and qual.operator == '=':
+				filedates.append(qual.value)
+				break
+		##for d in xrange(1,10):
+		##	yesterday = datetime.datetime.now() - datetime.timedelta(days = d)
+		##	self.download(yesterday.strftime('%Y%m%d'))
+		##files = glob.glob('/data/*.export.CSV.zip')
+		for filedate in filedates:
+			filepath = self.download(filedate)
+			if len(filepath) > 0:
+				with zipfile.ZipFile(filepath) as myzip:
+					with myzip.open(filepath[6:-4]) as stream:
+						reader = csv.reader(stream, delimiter='\t')
+						for line in reader:
+							yield line;

daa5fc04fe53557c0e6082750c910a29463eac45
Faraz Fallahi <fffaraz@gmail.com>
2019-03-04 18:54:27 -0600

    Update myscript.sh

--- data/myscript.sh
+++ data/myscript.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-echo "$@" > /data/111.txt
-printenv > /data/222.txt
-wget -q -O - "$@" "https://data.sfgov.org/api/views/yitu-d5am/rows.csv"
+wget -q -O "https://data.sfgov.org/api/views/yitu-d5am/rows.csv"

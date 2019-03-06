50ecfdae3c0fba03d652f914366b64c4990a8d3b
Faraz Fallahi <fffaraz@gmail.com>
2019-03-06 12:16:58 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
+ob_end_flush();
+ob_end_clean();
+ob_implicit_flush();
 for ($i=0; $i < 1000000; $i++)
 {
 	for ($j=0; $j < 58; $j++)
@@ -6,4 +9,5 @@ for ($i=0; $i < 1000000; $i++)
 		echo $j . "\t";
 	}
 	echo "\n";
+	flush();
 }

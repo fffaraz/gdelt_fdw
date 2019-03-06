6a5494afa402f6e0489743a6bbd02e722377ba3c
Faraz Fallahi <fffaraz@gmail.com>
2019-03-06 12:24:58 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,10 +1,7 @@
 <?php
-ini_set('max_execution_time', 0);
 set_time_limit(0);
 ob_end_flush();
-ob_end_clean();
-ob_implicit_flush();
-for ($i=0; $i < 1000; $i++)
+for ($i=0; $i < 10000; $i++)
 {
 	for ($j=0; $j < 58; $j++)
 	{

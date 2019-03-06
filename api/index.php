8434e1233395e0a6d0bb812de4dd4a60967f09d6
Faraz Fallahi <fffaraz@gmail.com>
2019-03-06 16:43:48 -0600

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,32 +5,31 @@ ini_set('memory_limit',-1);
 set_time_limit(0);
 ob_end_flush();
 
-$sqldate = '20190301';
-$filename = '/data/' . $sqldate . '.export.CSV.zip';
-if(!file_exists($filename)) file_put_contents($filename, fopen('http://data.gdeltproject.org/events/' . $sqldate . '.export.CSV.zip', 'r'));
-$zip = zip_open($filename);
-if(is_resource($zip))
+foreach(glob('/data/*.export.CSV.zip') as $filename)
 {
-	$zip_entry = zip_read($zip);
-	if($zip_entry && zip_entry_open($zip, $zip_entry, 'r'))
+	$sqldate = substr($filename, 6, -15);
+	if(!file_exists($filename)) file_put_contents($filename, fopen('http://data.gdeltproject.org/events/' . $sqldate . '.export.CSV.zip', 'r'));
+	$zip = zip_open($filename);
+	if(is_resource($zip))
 	{
-		$buffer = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
-		zip_entry_close($zip_entry);
-		$lines = explode("\r\n", $buffer);
-		foreach($lines as $line)
+		$zip_entry = zip_read($zip);
+		if($zip_entry && zip_entry_open($zip, $zip_entry, 'r'))
 		{
-			$csv = str_getcsv($line, "\t");
-			foreach($csv as $field)
+			$buffer = zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+			zip_entry_close($zip_entry);
+			$lines = explode("\r\n", $buffer);
+			foreach($lines as $line)
 			{
-				echo $field . "\t";
+				$csv = str_getcsv($line, "\t");
+				foreach($csv as $field)
+				{
+					echo $field . "\t";
+				}
+				echo "\n";
+				flush();
 			}
-			echo "\n";
-			flush();
 		}
+	    zip_close($zip);
 	}
-    zip_close($zip);
-}
-else
-{
-	echo "Error: " . zipFileErrMsg($zip);
+	//else echo "Error: " . zipFileErrMsg($zip);
 }

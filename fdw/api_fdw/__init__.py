f3a9c3db94e1cc3b7e8c393a9dbbfb28566537f2
Faraz Fallahi <fffaraz@gmail.com>
2019-03-06 12:14:40 -0600

    Update __init__.py

--- fdw/api_fdw/__init__.py
+++ fdw/api_fdw/__init__.py
@@ -11,13 +11,13 @@ class ApiForeignDataWrapper(ForeignDataWrapper):
 		self.options = options
 		self.columns = columns
 		self.url = options["url"]
+		self.param1 = urllib.urlencode(self.options)
+		self.param2 = 'colstable=' + ','.join(self.columns)
 
 	def execute(self, quals, columns):
-		param1 = urllib.urlencode(self.options)
-		param2 = 'colstable=' + ','.join(self.columns)
 		param3 = 'colsquery=' + ','.join(columns)
 		param4 = 'quals=' + ','.join([str(qual.field_name) + str(qual.operator) + str(qual.value) for qual in quals])
-		params = param1 + '&' + param2 + '&' + param3 + '&' + param4
+		params = self.param1 + '&' + self.param2 + '&' + param3 + '&' + param4
 		url = self.url + '?' + params
 		reader = csv.reader(urllib2.urlopen(url), delimiter='\t', quoting=csv.QUOTE_NONE)
 		for row in reader:

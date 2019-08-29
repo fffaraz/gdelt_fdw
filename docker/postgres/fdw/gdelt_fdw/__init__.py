a1fe96b5c72ecca6fdda56f4778b6ed29e7c6085
Faraz Fallahi <fffaraz@gmail.com>
2019-08-29 15:45:03 -0500

    Update __init__.py

--- docker/postgres/fdw/gdelt_fdw/__init__.py
+++ docker/postgres/fdw/gdelt_fdw/__init__.py
@@ -68,7 +68,7 @@ class GdeltForeignDataWrapper(ForeignDataWrapper):
 			for file in glob.glob('/data/*.export.CSV.zip'):
 				filedates.append(file[6:-15])
 
-		for filedate in list(set(filedates)).sort():
+		for filedate in sorted(list(set(filedates))):
 			filepath = self.download(filedate)
 			if len(filepath) > 0:
 				with zipfile.ZipFile(filepath) as myzip:
